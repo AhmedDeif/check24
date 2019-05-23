fce1fd29e05e4e3914dd761ca0d1ba080cb645f9
Doola <ahmed.adel.abodeif@gmail.com>
2019-05-23 09:59:06 +0200

    Product List View init

---
+++ AbodeifCheck24/AbodeifCheck24/Components/ProductListTable/ProductListFilterHeader.swift
@@ -0,0 +1,14 @@
+//
+//  ProductListFilterHeader.swift
+//  AbodeifCheck24
+//
+//  Created by Ahmed Abodeif on 5/23/19.
+//  Copyright © 2019 Ahmed Abodeif. All rights reserved.
+//
+
+import UIKit
+
+class ProductListFilterHeader: UITableViewHeaderFooterView {
+
+    
+}
---
+++ AbodeifCheck24/AbodeifCheck24/Components/ProductListTable/ProductListFooter.swift
@@ -0,0 +1,21 @@
+//
+//  ProductListFooter.swift
+//  AbodeifCheck24
+//
+//  Created by Ahmed Abodeif on 5/23/19.
+//  Copyright © 2019 Ahmed Abodeif. All rights reserved.
+//
+
+import UIKit
+
+class ProductListFooter: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
