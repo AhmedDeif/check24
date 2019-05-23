003d84a924accc841e8d77b434094f6cc27f6fb8
Doola <ahmed.adel.abodeif@gmail.com>
2019-05-23 09:40:46 +0200

    Project Structure init

---
+++ AbodeifCheck24/AbodeifCheck24/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AbodeifCheck24
+//
+//  Created by Ahmed Abodeif on 5/23/19.
+//  Copyright © 2019 Ahmed Abodeif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
