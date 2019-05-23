51740f4c2dc6c699fa3cab4afa84e47e66e230ee
Doola <ahmed.adel.abodeif@gmail.com>
2019-05-23 10:01:10 +0200

    Product table view cells

---
+++ AbodeifCheck24/AbodeifCheck24/Components/ProductListTable/FavoriteProductTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FavoriteProductTableViewCell.swift
+//  AbodeifCheck24
+//
+//  Created by Ahmed Abodeif on 5/23/19.
+//  Copyright © 2019 Ahmed Abodeif. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteProductTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ AbodeifCheck24/AbodeifCheck24/Components/ProductListTable/ProductTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProductTableViewCell.swift
+//  AbodeifCheck24
+//
+//  Created by Ahmed Abodeif on 5/23/19.
+//  Copyright © 2019 Ahmed Abodeif. All rights reserved.
+//
+
+import UIKit
+
+class ProductTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
